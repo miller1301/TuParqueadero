fae25d0d7350acc59cecf8bcbe5fa28c07904cb9
Miller Nivia <millernivia1301@gmail.com>
2022-02-18 15:13:36 -0500

    Creacioón componente homeUser

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './modules/user/home/home.component';
 
 const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
+  { 
+    path: '', 
+    component: HomeComponent 
+  }
 ];
 
 @NgModule({
